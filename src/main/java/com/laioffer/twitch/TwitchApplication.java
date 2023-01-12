654b49e1af5d7c5f26b405c20bfcce23e16f10b9
ALLEN XU <chaolunx@usc.edu>
2023-01-12 14:08:47 -0800

    first commit

---
+++ src/main/java/com/laioffer/twitch/TwitchApplication.java
@@ -0,0 +1,14 @@
+package com.laioffer.twitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitchApplication.class, args);
+    }
+
+}
+
